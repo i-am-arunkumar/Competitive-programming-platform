f81be8051bff507563e03da157b55c9ba451d9e0
gopikrishna <gopikrishna01012001@gmail.com>
2021-11-10 17:25:23 +0530

    home page

--- index.php
+++ index.php
@@ -1,19 +1,21 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <!-- JavaScript Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+    <style>
+        #header {
+            height: 55px;
+            margin: 10 11em;
+        }
 
-<style>
-    #header {
-    height: 55em;
-    margin: 10 11em;
-}
-
-/*
+        /*
 .menu-list-container {
     margin: 0;
     margin-top: 0px;
@@ -23,73 +25,171 @@
 }
 */
 
-#menu-bar {
-	height: 64px;
-	background-color: #2dc4cf27;
- 
-}
+        #menu-bar {
+            height: 64px;
+            background-color: #2dc4cf27;
 
-.menu-list-container {
-	display: table;   /* Allow the centering to work */
-	margin: 0 auto;
-    
-}
+        }
 
-.main-menu-list {
-	min-width: 696px;
-	list-style: none;
-	padding-top: 20px;
+        .menu-list-container {
+            display: table;
+            /* Allow the centering to work */
+            margin: 0 auto;
 
-	}
-	ul.main-menu-list li {
-		display: inline;
-	}
+        }
 
+        .main-menu-list {
+            min-width: 696px;
+            list-style: none;
+            padding-top: 20px;
 
-</style>
+        }
+
+        ul.main-menu-list li {
+            display: inline;
+        }
+
+        .card{
+            margin-top: 16px;
+        }
+
+    </style>
 
 </head>
+
 <body>
-   <?php
+    <?php
     //echo "<p>lol</p>";
-   ?>
+    ?>
 
-<div id="header" style="position: relative;">
-   <div style="float:left; height: 60px; ">
-                    <a href="http://localhost/Competitive-programming-platform/index.php"><img alt="Competitive Programming Platform" title="Thank You" src="./CP platform.png"  style="display: block; margin-left:auto; margin_right: auto; width: 60%; height:160px" ></a>
+
+    <nav style="position:fixed;width:100%;z-index:100;" class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">
+                <img src="https://th.bing.com/th/id/R.9c5c3d2ed1ebd22dbcf567bc5d2d4fc8?rik=vC8lSSfdD1U5zg&riu=http%3a%2f%2fwww.newdesignfile.com%2fpostpic%2f2014%2f09%2fcomputer-programming-code-icon_334973.png&ehk=opjpxA8O%2bpC5h%2bX8BO4YhrH6OlFCGHNEpuf8I9v9tqg%3d&risl=&pid=ImgRaw&r=0" alt="" width="48" height="48" class="d-inline-block align-text-center">
+                Competitive Programming
+            </a>
+            <!-- <a class="navbar-brand" href="#">Competitive Programming</a> -->
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="#">Leaderboard</a>
+                    </li>
+                </ul>
+                <form class="d-flex">
+                    <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                    <button class="btn btn-outline-success" type="submit">Search</button>
+                </form>
+                <button type="button" class="btn btn-primary" style="margin-left : 16px;" data-bs-toggle="modal" data-bs-target="#signindialog">SIGN IN</button>
+            </div>
+        </div>
+    </nav>
+
+    <!-- Sign in dialog -->
+    <div class="modal fade" id="signindialog" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="staticBackdropLabel">Sign in</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    <form>
+                        <div class="mb-3">
+                            <label for="exampleInputEmail1" class="form-label">Email address</label>
+                            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+                        </div>
+                        <div class="mb-3">
+                            <label for="exampleInputPassword1" class="form-label">Password</label>
+                            <input type="password" class="form-control" id="exampleInputPassword1">
+                        </div>
+                    </form>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-primary" type="submit">sign in</button>
+                </div>
+            </div>
+        </div>
     </div>
-    
-    <div id="login" style="text-align: right;">
-      <a href=""> sign in </a>
+
+
+    <div  style="padding-top:72px">
+
+        <div class="card">
+            <h5 class="card-header">Featured</h5>
+            <div class="card-body">
+                <h5 class="card-title">Special title treatment</h5>
+                <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                <a href="#" class="btn btn-primary">Go somewhere</a>
+            </div>
+        </div>
+        <div class="card">
+            <h5 class="card-header">Featured</h5>
+            <div class="card-body">
+                <h5 class="card-title">Special title treatment</h5>
+                <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                <a href="#" class="btn btn-primary">Go somewhere</a>
+            </div>
+        </div>
+        <div class="card">
+            <h5 class="card-header">Featured</h5>
+            <div class="card-body">
+                <h5 class="card-title">Special title treatment</h5>
+                <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                <a href="#" class="btn btn-primary">Go somewhere</a>
+            </div>
+        </div>
+        <div class="card">
+            <h5 class="card-header">Featured</h5>
+            <div class="card-body">
+                <h5 class="card-title">Special title treatment</h5>
+                <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                <a href="#" class="btn btn-primary">Go somewhere</a>
+            </div>
+        </div>
+        <div class="card">
+            <h5 class="card-header">Featured</h5>
+            <div class="card-body">
+                <h5 class="card-title">Special title treatment</h5>
+                <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                <a href="#" class="btn btn-primary">Go somewhere</a>
+            </div>
+        </div>
+        <div class="card">
+            <h5 class="card-header">Featured</h5>
+            <div class="card-body">
+                <h5 class="card-title">Special title treatment</h5>
+                <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                <a href="#" class="btn btn-primary">Go somewhere</a>
+            </div>
+        </div>
+        <div class="card">
+            <h5 class="card-header">Featured</h5>
+            <div class="card-body">
+                <h5 class="card-title">Special title treatment</h5>
+                <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                <a href="#" class="btn btn-primary">Go somewhere</a>
+            </div>
+        </div>
+        <div class="card">
+            <h5 class="card-header">Featured</h5>
+            <div class="card-body">
+                <h5 class="card-title">Special title treatment</h5>
+                <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                <a href="#" class="btn btn-primary">Go somewhere</a>
+            </div>
+        </div>
 
     </div>
-<br>
-
-    <div id="menu-bar">
-    <div class="menu-list-container">
-    <ul class="main-menu-list">
-                <li class="current"><a href="/">Home</a></li>
-                <li class=""><a href="/top">Top</a></li>
-                <li class=""><a href="/contests">Contests</a></li>
-                <li class=""><a href="/gyms">Gym</a></li>
-                <li class=""><a href="/problemset">Problemset</a></li>
-                <li class=""><a href="/groups">Groups</a></li>
-                <li class=""><a href="/ratings">Rating</a></li>
-                <li class=""><a href="/edu/courses"><span class="edu-menu-item">Edu</span></a></li>
-                <li class=""><a href="/apiHelp">API</a></li>
-                <li class=""><a href="/calendar">Calendar</a></li>
-                <li class=""><a href="/help">Help</a></li>
-    </ul>
-      <!--  <form method="post" action="/search"><input type="hidden" name="csrf_token" value="500463444a364ebfc680e2f21fe0ce44">
-            <input class="search" name="query" data-isplaceholder="true" value="">
-        <input type="hidden" name="_tta" value="415"></form>  -->
-    <br style="clear: both;">
-</div>
-</div>
-   
 
 
-    
+
+
+
 
 </body>
+
 </html>
