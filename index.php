c5e1cd1af5bf707c3a861d377e49058a212ddd3b
gopikrishna <gopikrishna01012001@gmail.com>
2021-11-11 19:20:26 +0530

    included header in index

--- index.php
+++ index.php
@@ -9,9 +9,8 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <!-- JavaScript Bundle with Popper -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
-
     <style>
-        .card{
+        .card {
             margin-top: 16px;
         }
     </style>
@@ -19,15 +18,14 @@
 </head>
 
 <body>
-    <?php
-    //echo "<p>lol</p>";
+    <?php    
+include("commons/header.php");
     ?>
 
 
 
+    <div style="padding-top:72px">
 
-    <div  style="padding-top:72px">
-
         <div class="card">
             <h5 class="card-header">Featured</h5>
             <div class="card-body">
@@ -84,21 +82,7 @@
                 <a href="#" class="btn btn-primary">Go somewhere</a>
             </div>
         </div>
-        <div class="card">
-            <h5 class="card-header">Featured</h5>
-            <div class="card-body">
-                <h5 class="card-title">Special title treatment</h5>
-                <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-                <a href="#" class="btn btn-primary">Go somewhere</a>
-            </div>
-        </div>
-
     </div>
-
-
-
-
-
 
 </body>
 
