dd03f49a4b7eb832447ca736c37161b536fa3e93
gopikrishna <gopikrishna01012001@gmail.com>
2021-11-11 23:45:51 +0530

    update

--- index.php
+++ index.php
@@ -18,71 +18,54 @@
 </head>
 
 <body>
-    <?php    
-include("commons/header.php");
+    <?php
+    include("commons/header.php");
     ?>
 
 
 
     <div style="padding-top:72px">
 
-        <div class="card">
-            <h5 class="card-header">Featured</h5>
-            <div class="card-body">
-                <h5 class="card-title">Special title treatment</h5>
-                <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-                <a href="#" class="btn btn-primary">Go somewhere</a>
-            </div>
-        </div>
-        <div class="card">
-            <h5 class="card-header">Featured</h5>
-            <div class="card-body">
-                <h5 class="card-title">Special title treatment</h5>
-                <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-                <a href="#" class="btn btn-primary">Go somewhere</a>
-            </div>
-        </div>
-        <div class="card">
-            <h5 class="card-header">Featured</h5>
-            <div class="card-body">
-                <h5 class="card-title">Special title treatment</h5>
-                <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-                <a href="#" class="btn btn-primary">Go somewhere</a>
-            </div>
-        </div>
-        <div class="card">
-            <h5 class="card-header">Featured</h5>
-            <div class="card-body">
-                <h5 class="card-title">Special title treatment</h5>
-                <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-                <a href="#" class="btn btn-primary">Go somewhere</a>
-            </div>
-        </div>
-        <div class="card">
-            <h5 class="card-header">Featured</h5>
-            <div class="card-body">
-                <h5 class="card-title">Special title treatment</h5>
-                <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-                <a href="#" class="btn btn-primary">Go somewhere</a>
-            </div>
-        </div>
-        <div class="card">
-            <h5 class="card-header">Featured</h5>
-            <div class="card-body">
-                <h5 class="card-title">Special title treatment</h5>
-                <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-                <a href="#" class="btn btn-primary">Go somewhere</a>
-            </div>
-        </div>
-        <div class="card">
-            <h5 class="card-header">Featured</h5>
-            <div class="card-body">
-                <h5 class="card-title">Special title treatment</h5>
-                <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-                <a href="#" class="btn btn-primary">Go somewhere</a>
-            </div>
-        </div>
+
+        <?php
+        $servername = "localhost";
+        $username = "root";
+        $password = "";
+        $dbname = "cp_site";
+
+        $conn = new mysqli($servername, $username, $password, $dbname);
+
+        if ($conn->connect_error) {
+            //  echo 'k';
+            die("Connection failed: " . $conn->connect_error);
+        }
+
+        $sql = "SELECT contest_name, contest_date, contest_duration,contest_author,contest_description FROM contest_details";
+        $result = $conn->query($sql);
+
+        if ($result !== false && $result->num_rows > 0) {
+            // output data of each row
+            while ($row = $result->fetch_assoc()) {
+                //  echo "id: " . $row["id"]. " - Name: " . $row["firstname"]. " " . $row["lastname"]. "<br>";
+
+                echo '<div class="card">
+    <h5 class="card-header">' . $row["contest_name"] . ' <p style="float:right">prepared by ' . $row["contest_author"] . ' at ' . date('M/d/Y',strtotime($row["contest_date"]))  . '</p></h5>
+    <div class="card-body">
+        <h5 class="card-title">' . $row["contest_author"] . '</h5>
+        <p class="card-text">' . $row["contest_description"] . '</p>
+        <a href="/competitive-programming-platform/Contest" class="btn btn-primary">Enter</a>
     </div>
+</div> ';
+            }
+        } else {
+            echo 'nol';
+        }
+
+        $conn->close();
+        ?>
+
+
+
 
 </body>
 
