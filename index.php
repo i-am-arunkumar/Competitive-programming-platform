f18110ca2cf10b579e8499c84eade5869b59dc7f
Moses Chandramouli <moseschand003@gmail.com>
2021-11-12 08:46:00 +0530

    commons: added head to header.php

--- index.php
+++ index.php
@@ -1,27 +1,18 @@
 <!DOCTYPE html>
 <html lang="en">
 
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
-    <script src="https://code.jquery.com/jquery-3.1.1.min.js" integrity="sha256-hVVnYaiADRTO2PzUGmuLJr8BLUSjGIZsDYGmIJLv2b8=" crossorigin="anonymous"></script>
+<?php
+include("commons/header.php");
+?>
 
-    <style>
-        .card {
-            margin-top: 16px;
-        }
-    </style>
-
-</head>
+<style>
+    .card {
+        margin-top: 16px;
+    }
+</style>
 
 <body>
-    <?php
-    include("commons/header.php");
-    ?>
+
 
 
 
